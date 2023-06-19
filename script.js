6813638b17794d07336cc12bc15a1e3b383e2cbe
samuelalecas <samuelalecas@gmail.com>
2023-06-19 22:48:19 +0200

    Complete animation transition

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ let currentIndex = 0;
 let prevBtn = document.getElementById("prev");
 let nextBtn = document.getElementById("next");
 
+let carousel;
 
-// Carga de carrousel de imágenes
-function loadCarousel(array){
+
+// FUNCIÓN: Carga de carrousel de imágenes
+function loadCarousel(array) {
     let screen = document.querySelector(".screen");
-    let carousel = document.createElement("div");
+    carousel = document.createElement("div");
     carousel.classList.add("carousel");
-    
-    for(let index in array){
+
+    for (let index in array) {
         console.log(index);
         let image = document.createElement("img");
         image.src = "img/" + array[index];
@@ -21,60 +23,59 @@ function loadCarousel(array){
     screen.append(carousel);
 }
 
+// GESTIÓN DE BOTONES
+
+// Posición inicial de botones
+prevBtn.disabled = true;
+
+// FUNCIÓN: Pulsación de botón
+prevBtn.addEventListener("click", () => {
+    if (currentIndex === 1) {
+        prevBtn.disabled = true;
+    } else {
+        isDisabled(nextBtn);
+    };
+    currentIndex--;
+    moveCarousel(DIRECTIONS.LEFT);
+})
+
+nextBtn.addEventListener("click", () => {
+    if (currentIndex === arrayOfImages.length - 2) {
+        nextBtn.disabled = true;
+    } else {
+        isDisabled(prevBtn);
+    };
+    currentIndex++;
+    moveCarousel(DIRECTIONS.RIGHT);
+})
+
+// FUNCIÓN: Comprobar si el botón está deshabilitado
+function isDisabled(button) {
+    if (button.disabled) {
+        button.disabled = false;
+    }
+}
+
+const DIRECTIONS = {
+    LEFT: Symbol(),
+    RIGHT: Symbol(),
+}
+
+// FUNCIÓN: Desplaza el carrusel
+function moveCarousel(direction) {
+    let valueToMove;
+
+    if (direction === DIRECTIONS.LEFT) {
+        valueToMove = 150;
+    }
+
+    if (direction === DIRECTIONS.RIGHT) {
+        valueToMove = -150;
+    }
+
+    let carouselLeftPos = carousel.offsetLeft;
+    carousel.style.left = (carouselLeftPos + valueToMove) + 'px';
+}
+
+// AL COMENZAR
 loadCarousel(arrayOfImages);
-
-// // Inserción de imagen inicial
-// let screen = document.querySelector(".screen");
-// let image = document.createElement("img");
-// image.src = "img/" + arrayOfImages[currentIndex];
-// image.classList.add("image")
-// screen.appendChild(image);
-
-// // Posición inicial de botones
-// prevBtn.disabled = true;
-
-// // FUNCIÓN: Pulsación de botón
-// prevBtn.addEventListener("click", () => {
-//     if (currentIndex === 1) {
-//         currentIndex--;
-//         updateScreen(currentIndex);
-//         prevBtn.disabled = true;
-//         return;
-//     };
-//     currentIndex--;
-//     updateScreen(currentIndex);
-
-//     isDisabled(nextBtn);
-// })
-
-// nextBtn.addEventListener("click", () => {
-//     if (currentIndex === arrayOfImages.length - 2) {
-//         currentIndex++;
-//         updateScreen(currentIndex);
-//         nextBtn.disabled = true;
-//         return;
-//     };
-//     currentIndex++;
-//     updateScreen(currentIndex);
-
-//     isDisabled(prevBtn);
-// })
-
-
-// // FUNCIÓN: Actualizar pantalla
-// function updateScreen(position) {
-//     image.src = "img/" + arrayOfImages[position];
-// }
-
-// // FUNCIÓN: Comprobar si el botón está deshabilitado
-// function isDisabled(button){
-//     if (button.disabled) {
-//         button.disabled = false;
-//     }
-// }
-
-// // Hacer zoom
-// image.addEventListener("click",()=>{
-//     const zoom = document.createElement("div");
-//     zoom.classList.add("zoom");
-// })
